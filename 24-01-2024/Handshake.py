ad932ebee757699062386ae474a7745c5113b1a7
VIJAYALAKSHMI V <96854008+Vijayalakshmivijayakumar@users.noreply.github.com>
2024-01-24 22:16:04 +0530

    Update Handshake.py

--- 24-01-2024/Handshake.py
+++ 24-01-2024/Handshake.py
@@ -25,20 +25,14 @@ import re
 import sys
 
 def handshake(n):
-    # Write your code here
     return sum(range(n))
-
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     t = int(input().strip())
 
     for t_itr in range(t):
         n = int(input().strip())
-
         result = handshake(n)
-
         fptr.write(str(result) + '\n')
-
     fptr.close()
 
