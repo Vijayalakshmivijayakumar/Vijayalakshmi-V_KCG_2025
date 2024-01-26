74afeee6a42b7ec0e10e7573899aae76d70470b7
VIJAYALAKSHMI V <96854008+Vijayalakshmivijayakumar@users.noreply.github.com>
2024-01-26 22:40:40 +0530

    Update filling jars.py

--- 26-01-2024/filling jars.py
+++ 26-01-2024/filling jars.py
@@ -23,6 +23,45 @@
 # Sample Output
 # 160
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def solve(n, operations):
+    output = 0
+    for o in operations:
+        output += o[2] * (o[1]-o[0]+1)
+    return output//n
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    operations = []
+
+    for _ in range(m):
+        operations.append(list(map(int, input().rstrip().split())))
+
+    result = solve(n, operations)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+
+
+
 
 
 
